c95083e218761651989296daf244e43483432be8
Rihards <kubilisr@gmail.com>
2020-09-04 19:40:58 +0300

    deploy.sh rewritten

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,14 +1,15 @@
-#!/bin/sh
-
-commit_website_files() {
-  git add .
-  git commit -m "Travis build: $TRAVIS_BUILD_NUMBER"
-}
-
-upload_files() {
-  git remote add origin-pages https://${GTOKEN}@github.com/${GUSER}/${GREPONAME}.git > /dev/null 2>&1
-  git push --quiet --set-upstream origin-pages master
-}
-
-commit_website_files
-upload_files
+#!/bin/bash
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+cd public
+if [ -n "$GITHUB_AUTH_SECRET" ]
+then
+    touch ~/.git-credentials
+    chmod 0600 ~/.git-credentials
+    echo $GITHUB_AUTH_SECRET > ~/.git-credentials
+    git config credential.helper store
+    git config user.email "kubilisr-blog-bot@users.noreply.github.com"
+    git config user.name "kubilisr-blog-bot"
+fi
+git add .
+git commit -m "Rebuild site"
+git push --force origin HEAD:master
